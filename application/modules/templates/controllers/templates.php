0fa82858ec252fc650a6549a211b7624aa2bc99f
kenny lau <kennyllau9@gmail.com>
2016-11-20 19:41:51 -0800

    first commit

---
+++ application/modules/templates/controllers/templates.php
@@ -0,0 +1,34 @@
+	<?php
+	class Templates extends MX_Controller 
+	{
+		function __construct()
+		{
+			parent::__construct();
+
+			// $this->load->library('form_validation');
+			// $this->form_validation->CI =& $this;
+		}
+
+		function test()
+		{
+			$data = '';
+			$this->public_bootstrap($data);
+		}
+
+		function public_bootstrap($data)
+		{
+			$this->load->view('public_bootstrap', $data);
+		}
+
+		function public_jqm($data)
+		{
+			$this->load->view('public_jqm', $data);
+		}
+
+		function admin($data)
+		{
+			$this->load->view('admin', $data);
+		}
+
+
+	}
